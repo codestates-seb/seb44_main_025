f5feab6658dde6a474552d918e70f05f63273a01
johyeran <a01026823164@gmail.com>
2023-07-04 13:57:42 +0900

    feat: performance service 애너테이션 추가

--- BE/MainProject/src/main/java/com/codestates/performance/service/PerformanceServiceImpl.java
+++ BE/MainProject/src/main/java/com/codestates/performance/service/PerformanceServiceImpl.java
@@ -1,7 +1,9 @@
 package com.codestates.performance.service;
 
 import com.codestates.performance.entity.Performance;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PerformanceServiceImpl implements PerformanceService{
     @Override
     public void createPerformance(Performance performance) {
