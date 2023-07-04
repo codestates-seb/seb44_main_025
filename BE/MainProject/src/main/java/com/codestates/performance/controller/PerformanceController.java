6d93812c666c9b341f0947a56c66e752035bb807
johyeran <a01026823164@gmail.com>
2023-07-04 17:44:00 +0900

    Refactor: 생성자 주입 방식 변경

--- BE/MainProject/src/main/java/com/codestates/performance/controller/PerformanceController.java
+++ BE/MainProject/src/main/java/com/codestates/performance/controller/PerformanceController.java
@@ -4,26 +4,24 @@ import com.codestates.performance.dto.PerformanceDto;
 import com.codestates.performance.entity.Performance;
 import com.codestates.performance.mapper.PerformanceMapper;
 import com.codestates.performance.service.PerformanceService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@Slf4j
 @RequestMapping("/performance")
 @RestController
+@RequiredArgsConstructor
 public class PerformanceController {
     private final PerformanceMapper mapper;
     private final PerformanceService performanceService;
-    public PerformanceController(PerformanceMapper mapper, PerformanceService performanceService) {
-        this.mapper = mapper;
-        this.performanceService = performanceService;
-    }
-    @GetMapping("/register")
+
+    /* 공연 생성 */
+    @PostMapping("/register")
     public void postPerformance(@RequestBody @Valid PerformanceDto.Post performanceDto) {
-        // 카테고리명, 공연명, 이미지, 날짜, 금액. 총 좌석, 설명, 위치, 아티스트를 저장하는 DTO
         Performance performance = mapper.performanceDtoToPerformance(performanceDto);
-        performanceService.createPerformance(performance);
+        Performance response = performanceService.createPerformance(performance);
     }
 }
