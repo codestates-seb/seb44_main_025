c0153eb22ad3312875d9af7c232e3df5f21fe274
mule-heo <zaq98114@gmail.com>
2023-07-04 17:40:50 +0900

    Initial commit

---
+++ client/src/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <div>Not Found</div>,
+    children: [
+      {
+        path: '/performances',
+        element: <div>공연 리스트</div>,
+      },
+      {
+        path: '/performances/:performanceId',
+        element: <div>공연 상세정보</div>,
+      },
+      {
+        path: '/performances/register',
+        element: <div>공연 등록</div>,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
