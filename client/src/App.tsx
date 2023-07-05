986d56bbb9eb8be6f0e171e0bde00c7980e6bcac
YeondongChoe <chldusehd789@gmail.com>
2023-07-05 16:35:06 +0900

    push

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,9 @@
 import { Outlet } from 'react-router-dom';
-import Artist from './components/artist';
-import Artistcontainer from './components/artistcontainer';
-import Artistmain from './components/artistmain';
-import ArtistreviewContainer from './components/artistreviewcontainer';
-import Artistreview from './components/artistreview';
 
 function App() {
   return (
     <div className="App">
       <Outlet />
-      <Artist />
-      <Artistreview />
-      <Artistcontainer />
-      <Artistmain />
-      <ArtistreviewContainer />
     </div>
   );
 }
