a872fd640d8aa1017174b1dede74cb9db130c1fb
AngryDoggaebi <aofhoa@gmail.com>
2023-07-05 15:38:19 +0900

    Feat: fe/headers

---
+++ client/src/components/Header/HeaderOnlyPrevious.tsx
@@ -0,0 +1,14 @@
+import Previous from '../../icons/PreviousIcon';
+import S from './Header.styled';
+
+const HeaderOnlyPrevious = () => {
+  return (
+    <S.Body>
+      <S.Nav>
+        <Previous />
+      </S.Nav>
+    </S.Body>
+  );
+};
+
+export default HeaderOnlyPrevious;
