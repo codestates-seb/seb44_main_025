16aa29d3113dd0ce49e48619be593ca93cad9db4
YeondongChoe <chldusehd789@gmail.com>
2023-07-05 14:09:19 +0900

    concertpreview

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import Concertpreview from './components/concertpreview';
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Outlet />
+      <Concertpreview />
     </div>
   );
 }
