5bdd0a0365577f2e776a633225a1a3df77862568
9xherry <heamin825@naver.com>
2023-07-06 17:21:06 +0900

    reservation 컨트롤러 기능 구현 수정

--- BE/MainProject/src/main/java/com/codestates/reservation/repository/ReservationRepository.java
+++ BE/MainProject/src/main/java/com/codestates/reservation/repository/ReservationRepository.java
@@ -1,2 +1,11 @@
-package com.codestates.reservation.repository;public interface ReservationRepository {
+package com.codestates.reservation.repository;
+
+import com.codestates.reservation.entity.Reservation;
+
+import java.util.Optional;
+
+public interface ReservationRepository {
+    Optional<Reservation> findById(Long reservationId); //예약 정보 반환
+    Reservation save(Reservation reservation);
+    void delete(Reservation reservation);
 }
