44946b31dfc88078b8fb4e3bd6b0d93d530fb948
9xherry <heamin825@naver.com>
2023-07-06 17:22:32 +0900

    ReservationMapper 구현

--- BE/MainProject/src/main/java/com/codestates/reservation/mapper/ReservationMapper.java
+++ BE/MainProject/src/main/java/com/codestates/reservation/mapper/ReservationMapper.java
@@ -1,2 +1,12 @@
-package com.codestates.reservation.mapper;public interface ReservationMapper {
+package com.codestates.reservation.mapper;
+
+import com.codestates.reservation.dto.ReservationDto;
+import com.codestates.reservation.entity.Reservation;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+//@Component
+public interface ReservationMapper {
+    Reservation reservationRequestDtoToReservation(ReservationDto.ReservationRequestDto reservationRequestDto);
+    ReservationDto.ReservationResponseDto reservationToReservationResponseDto(Reservation reservation);
 }
