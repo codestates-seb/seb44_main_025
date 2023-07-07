8a3bed640b8bfe116ba8a2b8174b4104ff98196c
johyeran <a01026823164@gmail.com>
2023-07-07 15:32:41 +0900

    Fix: reservation jpa repository 상속 추가 및 payment 엔티티 설정 추가

--- BE/MainProject/src/main/java/com/codestates/payment/entity/Payment.java
+++ BE/MainProject/src/main/java/com/codestates/payment/entity/Payment.java
@@ -2,10 +2,13 @@ package com.codestates.payment.entity;
 
 import com.codestates.reservation.entity.Reservation;
 
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 
+@Entity
 public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long paymentId;
     @OneToOne
     @JoinColumn(name="RESERVATION_ID")
     private Reservation reservation;
