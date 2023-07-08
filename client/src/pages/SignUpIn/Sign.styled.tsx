7e7d11292cf8c40fa77661dedf1f4d9cc2984bca
AngryDoggaebi <aofhoa@gmail.com>
2023-07-09 02:05:31 +0900

    Feat: 유효성 검사 및 중복 확인 로직 구현(함수 구현 제외)

--- client/src/pages/SignUpIn/Sign.styled.tsx
+++ client/src/pages/SignUpIn/Sign.styled.tsx
@@ -1,13 +1,24 @@
 import { styled } from 'styled-components';
+
+interface SignProps {
+  width?: 360 | 285;
+}
 const S = {
+  H1: styled.h1`
+    color: var(--font-white-color);
+    font-size: var(--heading1-font-size);
+    font-weight: var(--heading1-font-weight);
+  `,
   Main: styled.main`
     display: flex;
     justify-content: center;
   `,
   Container: styled.div`
+    min-height: calc(100vh - 50px);
     display: flex;
     flex-flow: column;
-    justify-content: center;
+    justify-content: space-around;
+    align-items: center;
     width: 390px;
     background-color: var(--theme-background-color);
   `,
@@ -15,6 +26,76 @@ const S = {
     color: var(--font-white-color);
     font-size: var(--p-large-medium-font-size);
     font-weight: var(--p-large-medium-font-weight);
+  `,
+  Input: styled.input<SignProps>`
+    border-radius: 100px;
+    outline: none;
+    width: ${props => (props.width ? `${props.width}px` : '360px')};
+    height: 48px;
+    color: black;
+    font-size: 16px;
+    font-weight: Medium;
+    line-height: 24px;
+    padding-left: 15px;
+    white-space: nowrap;
+    &:focus {
+      border: 1.5px solid #8520ca;
+    }
+  `,
+  IputNDuplicateCheck: styled.div`
+    display: flex;
+    flex-flow: row;
+    justify-content: center;
+  `,
+  Form: styled.form`
+    width: 360px;
+    display: flex;
+    flex-direction: column;
+    & {
+      gap: 30px;
+      /* 이메일, 비밀번호, 비밀번호확인, 닉네임을 묶는 div */
+      & > div {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        justify-content: center;
+        /* 각각의 이메일, 비밀번호, 비밀번호확인, 닉네임 */
+        & > div {
+          height: 100px;
+          width: 100%;
+          /* 인풋과 중복확인을 묶는 div */
+          & > div {
+            width: 100%;
+            display: flex;
+            flex-flow: row;
+            justify-content: space-between;
+            align-items: center;
+          }
+        }
+      }
+    }
+    & label {
+      color: var(--button-white-background-color);
+      margin-left: 12px;
+    }
+    & p {
+      color: var(--button-highlight-border-color);
+      font-size: var(--nav-font-size);
+      margin-left: 12px;
+    }
+    & button {
+      margin-left: auto;
+    }
+    & span {
+      color: var(--font-primary--color);
+    }
+  `,
+  ButtonSpan: styled.span`
+    cursor: pointer;
+    color: var(--font-primary--color);
+  `,
+  Span: styled.span`
+    color: var(--font-white-color);
   `,
 };
 export default S;
--- client/src/pages/SignUpIn/SignUpPage.tsx
+++ client/src/pages/SignUpIn/SignUpPage.tsx
@@ -1,6 +1,5 @@
 import S from './Sign.styled';
 import HeaderOnlyP from '../../components/Header/HeaderOnlyP';
-import { Input } from '../../components/Inputs/Inputs';
 import { ButtonPrimary160px } from '../../components/Buttons/Buttons';
 import { useRef, useState } from 'react';
 import axios from 'axios';
@@ -10,45 +9,85 @@ interface IForm {
   email: string;
   password: string;
   password_confirm: string;
+  nickname: string;
 }
 
 const SignUpPage = () => {
-  let [nickname, setNickname] = useState('');
+  /** 중복여부
+   * @true :중복되지 않음
+   * @false :중복됨
+   */
+  let [emailDuplication, setEmailDuplication] = useState(false);
+  let [nicknameDuplication, setNicknameDuplication] = useState(false);
+  /** 중복확인버튼 클릭 여부 */
+  let [emailDuplBtnCnt, setEmailDuplBtnCnt] = useState(0);
+  let [nicknameDuplBtnCnt, setNicknameDuplBtnCnt] = useState(0);
+  /** 중복확인 클릭하지 않고 submit 했을 때
+   * @ture :중복확인 X
+   * @false :중복확인 O
+   */
+  let [noEmailDuplBtnClickedSubmit, setNoEmailDuplBtnClickedSubmit] =
+    useState(true);
+  let [noNicknameDuplBtnClickedSubmit, setNoNicknameDuplBtnClickedSubmit] =
+    useState(true);
+  let [submitClicked, setSubmitClicked] = useState(false);
+  useState(true);
+
   const {
     register,
     watch,
     formState: { errors },
     handleSubmit,
   } = useForm<IForm>();
+
+  /** email 현재값 (중복확인시 사용)*/
+  const input_email = useRef<string | null>(null);
+  input_email.current = watch('email');
   /** 비밀번호 현재값 (비밀번호 확인란에서 사용)*/
   const input_password = useRef<string | null>(null);
   input_password.current = watch('password');
+  /** 닉네임 현재값 (중복확인시 사용)*/
+  const input_nickname = useRef<string | null>(null);
+  input_nickname.current = watch('nickname');
 
-  /** email, password 유효성 검사 정규식
-   * @email :
-   * [A-Za-z0-9] 영문 대소문자 또는 숫자로 시작,
-   * ([-_.]?[A-Za-z0-9]) 두 번째 글자부터 영문, 숫자, -_.사용가능
-   * * 숫자 또는 문자 0개 이상
-   * @ 필수
-   * . 필수
-   * [A-Za-z]{2,3} 도메인 2 또는 3글자
-   * @Password :
-   * [A-Za-z0-9]{8,20} 영문 또는 숫자 8~20 글자
+  /** 유효성 검사 정규식
+   * @email :@.필수, TDL 2또는 3글자
+   * @Password :8~16자 영문, 숫자 조합
+   * @nickname :한글 또는 영문 가능
    */
-  const emailRegEx =
+  const emailRegExp =
     /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/;
-  const passwordRegEx = /^[A-Za-z0-9]{8,20}$/;
+  const passwordRegExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,16}$/;
+  const nicknameRegExp = /^[a-zA-Zㄱ-힣][a-zA-Zㄱ-힣 ]*$/;
 
   /** 입력한 값들을 react-hook-form의 SubmitHandler를 통해 객체(data)로 받는 함수 */
   const onSubmit: SubmitHandler<IForm> = data => {
-    /** data로 받은 객체와 nicname을 묶어 회원정보를 보관하는 새로운 객체 생성 */
-    const userProfile = {
-      ...data,
-      nickname,
-    };
-    /** 회원정보를 서버로 전송하는 ajax */
+    setSubmitClicked(true);
+    if (!noEmailDuplBtnClickedSubmit && !noNicknameDuplBtnClickedSubmit) {
+      if (!emailDuplication && !nicknameDuplication) {
+        함수(data);
+        console.log(noNicknameDuplBtnClickedSubmit);
+      } else {
+        if (emailDuplication) {
+          if (emailDuplBtnCnt > 0) {
+            setNoEmailDuplBtnClickedSubmit(true);
+          }
+        }
+        if (nicknameDuplication) {
+          if (nicknameDuplBtnCnt > 0) {
+            setNoNicknameDuplBtnClickedSubmit(true);
+          }
+        }
+      }
+    }
+  };
+  /** 회원정보를 서버로 전송하는 ajax 함수 */
+  const 함수 = (data: IForm) => {
+    console.log(data);
+    console.log(emailDuplBtnCnt);
+    console.log(nicknameDuplBtnCnt);
     axios
-      .post('/signup', userProfile)
+      .post('/signup', data)
       .then(response => {
         const { accessToken } = response.data;
         axios.defaults.headers.common[
@@ -59,94 +98,144 @@ const SignUpPage = () => {
         alert(`error: ${error}`);
       });
   };
+  /** 중복 확인하는 함수
+   * @todo 중복확인 로직 구현
+   */
+  const duplicateCheck = (
+    email: string | null,
+    nickname: string | null
+  ): void => {
+    if (typeof email === 'string' && nickname === '') {
+      if (email === 'hello@naver.com') {
+        setEmailDuplication(false);
+        setEmailDuplBtnCnt(emailDuplBtnCnt + 1);
+        setNoEmailDuplBtnClickedSubmit(false);
+      } else {
+        setEmailDuplication(true);
+        setEmailDuplBtnCnt(emailDuplBtnCnt + 1);
+        setNoEmailDuplBtnClickedSubmit(false);
+      }
+    }
+    if (email === '' && typeof nickname === 'string') {
+      // 중복검사를 통과한 경우
+      if (nickname === 'hello') {
+        setNicknameDuplication(false);
+        setNicknameDuplBtnCnt(nicknameDuplBtnCnt + 1);
+        setNoNicknameDuplBtnClickedSubmit(false);
+      }
+      // 통과하지 못 한 경우
+      else {
+        setNicknameDuplication(true);
+        setNicknameDuplBtnCnt(nicknameDuplBtnCnt + 1);
+        setNoNicknameDuplBtnClickedSubmit(false);
+      }
+    }
+  };
 
   return (
     <>
       <HeaderOnlyP />
       <S.Main>
         <S.Container>
-          <form onSubmit={handleSubmit(onSubmit)}>
+          <S.H1>Ez to 회원가입</S.H1>
+          <S.Form onSubmit={handleSubmit(onSubmit)}>
             <div>
-              <input
-                {...register('email', {
-                  required: true,
-                  pattern: emailRegEx,
-                })}
-              />
-              {errors.email && (
-                <p style={{ color: 'white' }}>이메일 형식으로 입력해라</p>
-              )}
+              <div>
+                <label htmlFor="email">이메일</label>
+                <div>
+                  <S.Input
+                    width={285}
+                    {...register('email', {
+                      required: true,
+                      pattern: emailRegExp,
+                      onChange: e => setNoEmailDuplBtnClickedSubmit(true),
+                    })}
+                  />
+                  <S.ButtonSpan
+                    onClick={() => {
+                      duplicateCheck(input_email.current, '');
+                    }}
+                  >
+                    중복확인
+                  </S.ButtonSpan>
+                </div>
+                <div>{errors.email && <p>이메일 형식으로 입력해라</p>}</div>
+                {noEmailDuplBtnClickedSubmit ? (
+                  submitClicked === true ? (
+                    <p>중복확인을 해주세요</p>
+                  ) : null
+                ) : emailDuplication ? (
+                  <p>같은 이메일 이미 있음</p>
+                ) : emailDuplBtnCnt > 0 ? (
+                  <p style={{ color: 'var(--input-border-color)' }}>
+                    {' '}
+                    사용 가능한 이메일입니다
+                  </p>
+                ) : null}
+              </div>
+              <div>
+                <label htmlFor="password">비밀번호</label>
+                <S.Input
+                  value={'1234qwer'}
+                  type="password"
+                  {...register('password', {
+                    required: true,
+                    pattern: passwordRegExp,
+                  })}
+                />
+                {errors.password && <p>비밀번호 형식으로 입력해라</p>}
+              </div>
+              <div>
+                <label htmlFor="password_fonfirm">비밀번호 확인</label>
+                <S.Input
+                  value={'1234qwer'}
+                  type="password"
+                  {...register('password_confirm', {
+                    required: true,
+                    validate: value => value === input_password.current,
+                  })}
+                />
+                {errors.password_confirm && <p>똑같은 비밀번호 입력해라</p>}
+              </div>
+              <div>
+                <label htmlFor="nickname">닉네임</label>
+                <div>
+                  <S.Input
+                    width={285}
+                    {...register('nickname', {
+                      required: true,
+                      pattern: nicknameRegExp,
+                      onChange: e => setNoNicknameDuplBtnClickedSubmit(true),
+                    })}
+                  />
+                  <S.ButtonSpan
+                    onClick={() => {
+                      duplicateCheck('', input_nickname.current);
+                    }}
+                  >
+                    중복확인
+                  </S.ButtonSpan>
+                </div>
+                {errors.nickname && <p>닉네임 규칙에 맞게 입력해라</p>}
+                {noNicknameDuplBtnClickedSubmit ? (
+                  submitClicked === true ? (
+                    <p>중복확인을 해주세요</p>
+                  ) : null
+                ) : nicknameDuplication ? (
+                  <p>같은 닉네임 이미 있음</p>
+                ) : nicknameDuplBtnCnt > 0 ? (
+                  <p style={{ color: 'var(--input-border-color)' }}>
+                    사용 가능한 닉네임입니다
+                  </p>
+                ) : null}
+              </div>
             </div>
-            <div>
-              <input
-                type="password"
-                {...register('password', {
-                  required: true,
-                  pattern: passwordRegEx,
-                })}
-              />
-              {errors.password && (
-                <p style={{ color: 'white' }}>비밀번호 형식으로 입력해라</p>
-              )}
-            </div>
-            <div>
-              <input
-                type="password"
-                {...register('password_confirm', {
-                  required: true,
-                  validate: value => value === input_password.current,
-                })}
-              />
-              {errors.password_confirm && (
-                <p style={{ color: 'white' }}>똑같은 비밀번호 입력하셈</p>
-              )}
-            </div>
-            <Input
-              label="닉네임"
-              width={360}
-              height={48}
-              value={nickname}
-              setValue={setNickname}
-            />
-            <ButtonPrimary160px
-              onClick={() => {
-                onSubmit;
-              }}
-            >
-              버튼
-            </ButtonPrimary160px>
-          </form>
-          {/* <Input
-            label="이메일로 회원가입하기"
-            width={360}
-            height={48}
-            value={email}
-            setValue={setEmail}
-          />
-          <Input label="비밀번호" width={360} height={48} />
-          <Input
-            label="비밀번호 확인"
-            width={360}
-            height={48}
-            value={password}
-            setValue={setPwassword}
-          />
-          <Input
-            label="닉네임"
-            width={360}
-            height={48}
-            value={nickname}
-            setValue={setNickname}
-          />
-          <ButtonPrimary160px
-            onClick={() => {
-              registerFC();
-            }}
-          >
-            회원가입하기
-          </ButtonPrimary160px>
-          <S.P>이미 계정이 있으신가요? </S.P>
-          <S.P>로그인</S.P> */}
+            <ButtonPrimary160px>회원가입하기</ButtonPrimary160px>
+          </S.Form>
+          <div>
+            <S.Span>이미 계정이 있으신가요?</S.Span>
+            <S.ButtonSpan>로그인</S.ButtonSpan>
+          </div>
         </S.Container>
       </S.Main>
     </>
