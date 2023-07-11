fa48a6dbca96947ac5d060769fef14b4b60480f3
AngryDoggaebi <120698922+AngryDoggaebi@users.noreply.github.com>
2023-07-11 15:03:24 +0900

    Feat fe/signup(login) Form & API
    * Feat: 회원가입페이지 유효성 검사 및 ajax로직 구현
    
    * Fix: index.tsk conflict
    
    * Feat: 유효성 검사 및 중복 확인 로직 구현(함수 구현 제외)
    
    * Refactor: 안내문구 수정
    
    * Refactor: 안내문구 수정
    
    * Refactor: 하단 링크 컴포넌트 분리, useState => zustand 변경
    
    * Feat: 로그인 페이지 구현
    
    * Refactor: ajax 각각의 기능에 맞는 형태로 수정
    
    * Feat: 내용 space-around 에서 center로 변경
    
    * Init: React-cookie install
    
    * Feat: input 사이즈 조절, 로그인-비밀번호 error message 추가
    
    * Fix
    
    * Refactor: 정규표현식 수정, error message 변경
    
    * Feat: 중복확인 로직 구현
    
    * Feat: mian api 분리파일 생성(임시) 및 로그인 성공시 alert 추가
    
    * Fix: 메인페이지 navbar 로그인으로 연결되는 navbar로 변경, 오타수정
    
    * Feat: 중복검사 ajax 로직 구현
    
    * Feat: 회원가입 페이지 멀티플 에러 핸들링 적용

--- client/src/utils/RegExp.tsx
+++ client/src/utils/RegExp.tsx
@@ -1,11 +1,12 @@
 /** 유효성 검사 정규식 */
 
-/** @email :@.필수, TDL 2또는 3글자 */
+/** @email :@.필수, TDL 2또는 3자*/
 export const emailRegExp =
   /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/;
 
-/** @Password :8~16자 영문, 숫자 조합 */
-export const passwordRegExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,16}$/;
+/** @Password :8 ~ 16자 영문, 숫자, 특수문자를 최소 한가지씩 조합 */
+export const passwordRegExp =
+  /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/;
 
-/** @nickname :한글 또는 영문 가능*/
-export const nicknameRegExp = /^[a-zA-Zㄱ-힣][a-zA-Zㄱ-힣 ]*$/;
+/** @nickname :한글, 영문 가능, 띄어쓰기 불가*/
+export const nicknameRegExp = /^[a-zA-Zㄱ-힣][a-zA-Zㄱ-힣]*$/;
