06857cede289afa921520a306469058fa16628bc
YeondongChoe <125066377+YeondongChoe@users.noreply.github.com>
2023-07-13 10:19:44 +0900

    Feat fe/artistregistpage
    * Feat: 아티스트페이지 기능구현
    
    * Fix: 파일 폴터 이름 변경
    
    * Feat: 아트스트페이지 기능구현
    
    * Feat: json서버로  post 요청  후 get 요청 기능구현
    
    * Feat: 아티스트 등록시 이미지 파일 업로드

--- client/src/pages/ArtistRegister.tsx
+++ client/src/pages/ArtistRegister.tsx
@@ -1,13 +1,69 @@
 import { styled } from 'styled-components';
 import HeaderOnlyP from '../components/header/HeaderOnlyP';
-import { ButtonPrimary75px } from '../components/buttons/Buttons';
+import {
+  ButtonPrimary75px,
+  ButtonMiniToggleSelect,
+  ButtonMiniToggleUnselect,
+} from '../components/buttons/Buttons';
 import { Input, InputWithButton } from '../components/inputs/Inputs';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Img from '.././images/우리사랑이대로.jpeg';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+interface bodyType {
+  [x: string]: any;
+}
+
+const categoryObj = {
+  팝: 1,
+  락: 2,
+  'R&B': 3,
+  재즈: 4,
+  밴드: 5,
+  댄스: 6,
+};
 
 export default function Artistregist() {
-  const [Artist, SetArtist] = useState('');
-  const [Video, SetVideo] = useState('');
+  const [Artist, setArtist] = useState('');
+  const [Snslink, setSnslink] = useState('');
+  const [Introduction, setIntroduction] = useState('');
+  const [Video, setVideo] = useState('');
+
+  const [categoryId, setCategoryId] = useState<number | null>(null);
+  const handleClickCategory = (id: number) => {
+    if (categoryId === id) setCategoryId(null);
+    else setCategoryId(id);
+  };
+
+  const profileImgInputRef = useRef<HTMLInputElement>(null);
+  const backImgInputRef = useRef<HTMLInputElement>(null);
+  const [backImagesrc, setBeckImagesrc] = useState<string>(Img);
+  const [profileImagesrc, setProfileImagesrc] = useState<string>(Img);
+  const [backImgFile, setBackImgFile] = useState<Blob>();
+  const [profileImgFile, setProfileImgFile] = useState<Blob>();
+
+  const body = {
+    artistname: Artist,
+    snslink: Snslink,
+    content: Introduction,
+    categoryId: categoryId,
+    backImagesrc: backImgFile,
+    profileImagesrc: profileImgFile,
+  };
+  // json.server사용할때 id값이 꼭 있어야 함
+  const handleSubmit = async (body: bodyType) => {
+    const data = await axios
+      .post('http://localhost:5000/artist', JSON.stringify(body), {
+        headers: { 'Content-Type': 'application/json' },
+      })
+      .then(data => {
+        return data;
+      })
+      .catch(err => console.error(err));
+
+    return data;
+  };
 
   return (
     <>
@@ -15,31 +71,115 @@ export default function Artistregist() {
       <S.Main>
         <S.Section>
           <S.Title>아티스트 등록하기</S.Title>
-          <S.ProfileImg src={Img} />
-          <S.UserImg src={Img} />
+          <S.FileInput
+            type="file"
+            accept="image/*"
+            ref={backImgInputRef}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              const files = e.target.files;
+              if (files === undefined || files === null) return;
+              const file = files[0];
+              console.log(file);
+              setBackImgFile(file);
+              if (file) {
+                const reader = new FileReader();
+                reader.onload = e => {
+                  if (!e.target?.result || e.target?.result === null) return;
+                  setBeckImagesrc(e.target?.result as string);
+                };
+                reader.readAsDataURL(file);
+              }
+            }}
+          />
+          <S.ProfileImg
+            src={backImagesrc}
+            onClick={() => backImgInputRef.current?.click()}
+          ></S.ProfileImg>
+
+          <S.FileInput
+            type="file"
+            accept="image/*"
+            ref={profileImgInputRef}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              const files = e.target.files;
+              if (files === undefined || files === null) return;
+              const file = files[0];
+              console.log(file);
+              setProfileImgFile(file);
+              if (file) {
+                const reader = new FileReader();
+                reader.onload = e => {
+                  if (!e.target?.result || e.target?.result === null) return;
+                  setProfileImagesrc(e.target?.result as string);
+                };
+                reader.readAsDataURL(file);
+              }
+            }}
+          />
+          <S.UserImg
+            src={profileImagesrc}
+            onClick={() => profileImgInputRef.current?.click()}
+          ></S.UserImg>
           <S.ArtistDetail>
             <S.SubTitle>아티스트 정보</S.SubTitle>
             <S.ButtonWarppar>
-              <ButtonPrimary75px>등록</ButtonPrimary75px>
+              <Link to={'artist'}>
+                <ButtonPrimary75px onClick={() => handleSubmit(body)}>
+                  등록
+                </ButtonPrimary75px>
+              </Link>
             </S.ButtonWarppar>
+            <S.CategoryContainer>
+              {Object.keys(categoryObj).map((key, idx) => {
+                return idx === categoryId ? (
+                  <ButtonMiniToggleSelect
+                    key={idx}
+                    value={idx}
+                    onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+                      const value = +(e.target as HTMLInputElement).value;
+                      handleClickCategory(value);
+                    }}
+                  >
+                    {key}
+                  </ButtonMiniToggleSelect>
+                ) : (
+                  <ButtonMiniToggleUnselect
+                    key={idx}
+                    value={idx}
+                    onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+                      const value = +(e.target as HTMLInputElement).value;
+                      handleClickCategory(value);
+                    }}
+                  >
+                    {key}
+                  </ButtonMiniToggleUnselect>
+                );
+              })}
+            </S.CategoryContainer>
             <S.InputContainer>
               <S.InputLabel>아티스트</S.InputLabel>
               <InputWithButton
-                value={Artist}
+                // value={Artist}
                 icon={true}
                 width={285}
                 height={30}
                 buttonText={'중복확인'}
+                setValue={setArtist}
               ></InputWithButton>
             </S.InputContainer>
             <S.InputContainer>
               <S.InputLabel>SNS Link</S.InputLabel>
-              <Input height={30}></Input>
+              <Input height={30} setValue={setSnslink}></Input>
             </S.InputContainer>
             <S.InputContainer>
               <S.ArtistIntrodution>
                 <S.InputLabel>아티스트 소개</S.InputLabel>
-                <S.ArtistIntrodutionTextarea />
+                {/* 스타일드 컴포넌트는 value값을 받을 때 onChange로 받기 */}
+                <S.ArtistIntrodutionTextarea
+                  onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+                    setIntroduction(e.target.value)
+                  }
+                />
               </S.ArtistIntrodution>
             </S.InputContainer>
             <S.InputContainer>
@@ -86,9 +226,19 @@ const S = {
     margin-top: -40px;
   `,
   ProfileImg: styled.img`
-    width: 390px;
+    position: relative;
+    margin-left: 15px;
+    width: 360px;
     height: 150px;
-    padding: 0px 15px;
+    background-image: linear-gradient(#fff, #fff);
+    cursor: pointer;
+    object-fit: fill;
+    &[src] {
+      background-color: transparent;
+    }
+  `,
+  FileInput: styled.input`
+    display: none;
   `,
   UserImg: styled.img`
     position: relative;
@@ -109,6 +259,20 @@ const S = {
     background-origin: border-box;
     background-clip: content-box, border-box;
     z-index: 1;
+    cursor: pointer;
+    object-fit: scale-down;
+    &[src] {
+      background-color: transparent;
+    }
+  `,
+  CategoryContainer: styled.div`
+    margin: 20px 0px 20px 0px;
+    display: flex;
+    min-width: 360px;
+    justify-content: center;
+    & > button:not(:first-child) {
+      margin-left: 15px;
+    }
   `,
   ArtistDetail: styled.div`
     margin-top: 20px;
@@ -141,5 +305,6 @@ const S = {
     height: 100px;
     resize: none;
     border-radius: 15px;
+    padding: 10px;
   `,
 };
