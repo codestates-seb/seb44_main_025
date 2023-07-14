26da8cf2146a80e9b751a3fc0d9e58a42f08b2a2
mule-heo <zaq98114@gmail.com>
2023-07-14 18:40:23 +0900

    Feat: 요청 함수 업데이트

--- client/src/api/useFetch.ts
+++ client/src/api/useFetch.ts
@@ -5,17 +5,27 @@ import { useNavigate } from 'react-router-dom';
 const TEST_HOST = process.env.REACT_APP_TEST_HOST;
 const SERVER_HOST = process.env.REACT_APP_SERVER_HOST;
 
-interface PerformanceProps {
-  id: number;
+interface PerformanceType {
+  performanceId: number;
   title: string;
   date: string;
   price: number;
-  leftSeat: number;
-  content: string;
+  content: {
+    contentId: number;
+    body: string;
+  };
+  category: string;
   imageUrl: string;
+  performanceArtist: {
+    performanceId: number;
+    performanceArtistList: {
+      [x: string | number]: number;
+    };
+  };
+  place: string;
   categoryId: number;
-  // totalSeat 임시로 사용
   totalSeat: number;
+  leftSeat: number;
 }
 
 export const useGetMain = () => {
@@ -36,20 +46,25 @@ export const useGetMain = () => {
   return data;
 };
 
+interface AnyType {
+  [x: string | number]: any;
+}
 export const useTestGetPerformance = (id: string | number | undefined) => {
   if (id === undefined) return;
   const navigate = useNavigate();
-  const [data, setData] = useState<PerformanceProps>();
+  const [data, setData] = useState<AnyType>();
 
   const getData = async () => {
     await axios
-      .get<PerformanceProps>(`${TEST_HOST}/performances/${id}`)
-      .then(data => setData(data.data))
+      .get<AnyType>(`${SERVER_HOST}/performance/${id}`, {
+        headers: { 'ngrok-skip-browser-warning': true },
+      })
+      .then(data => setData(data?.data))
       .catch(err => {
         console.log(err);
         // TODO: 정보를 가져오지 못했을 시 Not Found 페이지 표시
         // 비정상적으로 접근한 페이지를 history에 남기지 않기 위해 replace 사용
-        navigate('/404', { replace: true });
+        // navigate('/404', { replace: true });
       });
   };
   useEffect(() => {
@@ -59,12 +74,21 @@ export const useTestGetPerformance = (id: string | number | undefined) => {
   return data;
 };
 
+interface PerformanceListType {
+  data: PerformanceType[];
+  pageInfo: {
+    page: number;
+    size: number;
+    total_elements: number;
+    total_pages: number;
+  };
+}
 export const useTestGetPerformances = () => {
-  const [data, setData] = useState<PerformanceProps[]>();
+  const [data, setData] = useState<PerformanceListType>();
 
   const getData = async () => {
     await axios
-      .get<PerformanceProps[]>(`${TEST_HOST}/performances`, {
+      .get<PerformanceListType>(`${SERVER_HOST}/performance?page=1&size=10`, {
         headers: { 'ngrok-skip-browser-warning': true },
       })
       .then(data => setData(data.data))
@@ -78,11 +102,29 @@ export const useTestGetPerformances = () => {
 };
 
 export const useGetPerformance = (id: string | number | undefined) => {
-  const [data, setData] = useState<PerformanceProps>();
+  const [data, setData] = useState<PerformanceType>();
 
   const getData = async () => {
     await axios
-      .get<PerformanceProps>(`${TEST_HOST}/performances/${id}`, {
+      .get<PerformanceType>(`${TEST_HOST}/performances/${id}`, {
+        headers: { 'ngrok-skip-browser-warning': true },
+      })
+      .then(data => setData(data.data))
+      .catch(err => console.log(err));
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return data;
+};
+
+export const useGetArtists = () => {
+  const [data, setData] = useState<AnyType>();
+
+  const getData = async () => {
+    await axios
+      .get<AnyType>(`${SERVER_HOST}/artist?category=1&page=1&size=10`, {
         headers: { 'ngrok-skip-browser-warning': true },
       })
       .then(data => setData(data.data))
