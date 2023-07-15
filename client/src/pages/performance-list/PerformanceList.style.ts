74c97dd20ae6aee04f69a298a69a7806d1d4bf58
mule-heo <zaq98114@gmail.com>
2023-07-16 05:17:55 +0900

    Fix: height 수정

--- client/src/pages/performance-list/PerformanceList.style.ts
+++ client/src/pages/performance-list/PerformanceList.style.ts
@@ -30,7 +30,7 @@ export const S = {
   `,
   Main: styled.main`
     width: 390px;
-    min-height: calc(100vh - 110px);
+    min-height: calc(100vh - 120px);
     background-color: var(--theme-background-color);
     display: flex;
     flex-direction: column;
