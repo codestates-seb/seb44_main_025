621016627f2fb8576e2877b87fce7569ae20a686
mule-heo <zaq98114@gmail.com>
2023-07-15 23:30:42 +0900

    Style: Inputs 스타일 정리

--- client/src/components/inputs/Inputs.style.ts
+++ client/src/components/inputs/Inputs.style.ts
@@ -1,5 +1,68 @@
-import { styled } from 'styled-components';
-import { InputStyle } from './Theme';
+import { styled, css } from 'styled-components';
+
+const InputStyle = {
+  dark: css`
+    background-color: rgba(198, 193, 204, 0.2);
+    & input {
+      color: white;
+    }
+    & svg {
+      stroke: white;
+
+      &:last-child {
+        margin-right: 16px;
+      }
+    }
+  `,
+  light: css`
+    background-color: white;
+    &:focus-within {
+      outline: 1.5px solid #8520ca;
+    }
+    & svg {
+      stroke: #1a1a1a;
+
+      &:last-child {
+        margin-right: 16px;
+      }
+    }
+  `,
+  warning: css`
+    background-color: rgba(229, 43, 103, 0.05);
+    border: 1.5px solid #e52b67;
+    & input {
+      color: white;
+    }
+  `,
+  success: css`
+    background-color: rgba(77, 207, 192, 0.05);
+    border: 1.5px solid #4dcfc0;
+    & input {
+      color: white;
+    }
+    & svg {
+      stroke: white;
+
+      &:last-child {
+        margin-right: 16px;
+      }
+    }
+  `,
+  primary: css`
+    background-color: transparent;
+    border: 2px solid var(--button-primary-background-color);
+    & input {
+      color: white;
+    }
+    & svg {
+      stroke: white;
+
+      &:last-child {
+        margin-right: 16px;
+      }
+    }
+  `,
+};
 
 // Label, 메시지까지 포함한 컨테이너 div
 export const Div = styled.div`
