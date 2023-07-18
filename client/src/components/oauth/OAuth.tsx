a664d460948b8083f2bcf37538049955be30aaad
AngryDoggaebi <aofhoa@gmail.com>
2023-07-18 16:49:41 +0900

    Fix: 안 쓰는 파일 삭제, 구글로그인시 홈 화면으로 이동하도록 수정

--- client/src/components/oauth/OAuth.tsx
+++ client/src/components/oauth/OAuth.tsx
@@ -1,13 +1,16 @@
 import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 const GoogleLoginButton = () => {
   const clientId: string = process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID || '';
+  const navigate = useNavigate();
   return (
     <>
       <GoogleOAuthProvider clientId={clientId}>
         <GoogleLogin
           onSuccess={res => {
             console.log(res);
+            navigate('/');
           }}
           onError={() => {
             console.log('Login Failed');
