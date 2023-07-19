6fa0428e585e28366a4a49c744c37d934d278166
mule-heo <zaq98114@gmail.com>
2023-07-20 04:07:28 +0900

    Feat: 공연 수정 페이지 추가

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,7 +7,6 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import axios from 'axios';
 import Notfoundpage from './pages/NotFound';
 import PerformanceList from './pages/performance-list/PerformanceList';
-import PerformanceInfo from './pages/performance-info/PerformanceInfo';
 import PerformanceRegister from './pages/performance-register/PerformanceRegister';
 import Artistpage from './pages/artist/Artist';
 import Mypage from './pages/mypage/MyPage';
@@ -20,6 +19,7 @@ import ArtistList from './pages/artist-list/ArtistList';
 import Home from './pages/home/Home';
 import ReviewRegister from './pages/review/ReviewRegister';
 import Artistedit from './pages/artist-register/ArtistEdit';
+import { Performance } from './pages/performance/Performance';
 
 /** @todo 도메인 주소 입력 */
 // axios.defaults.baseURL = '';
@@ -38,7 +38,7 @@ const router = createBrowserRouter([
       },
       {
         path: '/performances/:performanceId',
-        element: <PerformanceInfo />,
+        element: <Performance />,
       },
       {
         path: '/performances/register',
@@ -82,7 +82,7 @@ const router = createBrowserRouter([
         element: <Cancel />,
       },
       {
-        path: '/performances/review/write/:performanceId',
+        path: '/performances/:performanceId/review',
         element: <ReviewRegister />,
       },
     ],
