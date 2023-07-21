801c680301fbe1031e92b774136f79a6f6fd4fdc
AngryDoggaebi <120698922+AngryDoggaebi@users.noreply.github.com>
2023-07-21 10:40:42 +0900

    Feat fe/slogan (#167)
    캐러셀 형식 슬로건 구현

--- client/src/components/slogan/Slogan.styled.ts
+++ client/src/components/slogan/Slogan.styled.ts
@@ -2,18 +2,45 @@ import { styled } from 'styled-components';
 import { DeviceQuery } from '../../utils/Media';
 import { screenScale } from '../../utils/MediaSize';
 
+interface ContainerProps {
+  translate: string;
+  transform: string;
+}
+
 export const Styled_Slogan = {
   Container: styled.div`
     display: flex;
     justify-content: center;
-    align-items: center;
+    align-items: start;
     width: 390px;
     height: 60px;
-    border-radius: 30px;
-    background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
-      url('./images/단체사진.jpg');
     background-size: cover;
-    background-position: center;
+    border: 0.5px solid grey;
+    border-radius: 30px;
+
+    overflow: hidden;
+
+    ${DeviceQuery.tablet`
+      width: calc(390px * ${screenScale.tablet});
+      height: calc(60px * ${screenScale.tablet});
+    `}
+  `,
+  ImgGroup: styled.div<ContainerProps>`
+    display: flex;
+    flex-flow: column;
+    width: 390px;
+    transform: ${props => props.translate};
+    transition: ${props => props.transform};
+
+    ${DeviceQuery.tablet`
+      width: calc(390px * ${screenScale.tablet});
+    `}
+  `,
+  Img: styled.img`
+    width: 390px;
+    height: 60px;
+    background-color: var(--font-white-color);
+    vertical-align: bottom;
 
     ${DeviceQuery.tablet`
       width: calc(390px * ${screenScale.tablet});
--- client/src/components/slogan/Slogan.tsx
+++ client/src/components/slogan/Slogan.tsx
@@ -1,9 +1,87 @@
+import { useEffect, useState } from 'react';
 import { Styled_Slogan } from './Slogan.styled';
+import { useNavigate } from 'react-router-dom';
+import { useUserInfo } from '../../zustand/userInfo.stores';
 
 const Slogan = () => {
+  let [movement, setMovement] = useState(0);
+  let [time, setTime] = useState(0.5);
+  const mobileSizeHeight = 60; // 슬로건 모바일 높이
+  const tabletSizeHeight = 60 * 1.5; // 캐러셀 타블렛 높이
+  const tabletbifurcationPoint = 820; // 타블렛분기점
+  let [carouselHeight, setCarouselHeight] = useState(
+    window.innerWidth >= tabletbifurcationPoint
+      ? tabletSizeHeight
+      : mobileSizeHeight
+  ); // 첫 로딩시 슬로건 크기 초기값 세팅
+  const { userInfo2 } = useUserInfo();
+
+  /** 슬로건 4초마다 동작 */
+  useEffect(() => {
+    const interval = setInterval(() => {
+      SlideTransitionControl();
+    }, 4000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [movement]);
+
+  /** 슬로건 슬라이드 좌표 지정하는 함수 */
+  function SlideTransitionControl() {
+    // 마지막 슬라이드가 아닌 경우
+    if (movement !== -1 * carouselHeight) {
+      setTime(0.5);
+      setMovement(movement - carouselHeight);
+    }
+    // 마지막 슬라이드인 경우
+    else {
+      /** 마지막 슬라이드에서 첫 슬라이드로 */
+      // 0.5초동안 슬라이드 넘김
+      setMovement(movement - carouselHeight);
+      // 0.5초 후, 0초동안 첫 번째 슬라이드로 이동(사람 눈엔 보이지 않음)
+      setTimeout(function () {
+        setMovement(0);
+        setTime(0);
+      }, 500);
+    }
+  }
+
   return (
     <Styled_Slogan.Container>
-      <div></div>
+      <Styled_Slogan.ImgGroup
+        translate={`translateY(${movement}px)`}
+        transform={`transform ${time}s`}
+      >
+        {/* <Styled_Slogan.Img src="./images/2b3fad00c203b730.png" />
+        <Styled_Slogan.Img src="./images/600bb2ac964c4a46.png" />
+        <Styled_Slogan.Img src="./images/2b3fad00c203b730.png" /> */}
+
+        <a
+          href={
+            userInfo2?.hasArtist === true
+              ? '/performances/register'
+              : userInfo2?.hasArtist === false
+              ? '/artistregist'
+              : '/login'
+          }
+        >
+          <Styled_Slogan.Img src="./images/dd28119305b114d0.png" />
+        </a>
+        <a href={userInfo2?.hasArtist === false ? '/artistregist' : '/login'}>
+          <Styled_Slogan.Img src="./images/7955f4c5e4d540fb.png" />
+        </a>
+        <a
+          href={
+            userInfo2?.hasArtist === true
+              ? '/performances/register'
+              : userInfo2?.hasArtist === false
+              ? '/artistregist'
+              : '/login'
+          }
+        >
+          <Styled_Slogan.Img src="./images/dd28119305b114d0.png" />
+        </a>
+      </Styled_Slogan.ImgGroup>
     </Styled_Slogan.Container>
   );
 };
