9f728989fb3ca26a13b3244d69b0cf8cf4af345c
mule-heo <zaq98114@gmail.com>
2023-07-26 12:38:18 +0900

    Style: 카테고리 순서 변경

--- client/src/utils/Category.ts
+++ client/src/utils/Category.ts
@@ -2,18 +2,18 @@ export const categoryObj: { [x: string | number]: string } = {
   1: '팝',
   2: '락',
   3: 'R&B',
-  4: '힙합',
-  5: '재즈',
-  6: '밴드',
-  7: '댄스',
+  4: '재즈',
+  5: '밴드',
+  6: '댄스',
+  // 7: '힙합',
 };
 
 export const categoryIdObj: { [x: string]: number } = {
   팝: 1,
   락: 2,
   'R&B': 3,
-  힙합: 4,
-  재즈: 5,
-  밴드: 6,
-  댄스: 7,
+  재즈: 4,
+  밴드: 5,
+  댄스: 6,
+  // 힙합: 7,
 };
