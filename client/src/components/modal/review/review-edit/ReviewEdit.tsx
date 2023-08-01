03b36622d927f860217673159489fc0ccaab93f0
mule-heo <zaq98114@gmail.com>
2023-08-01 21:35:10 +0900

    Fix: 후기 등록 시 content 공백 여부 검사 로직 변경

--- client/src/components/modal/review/review-edit/ReviewEdit.tsx
+++ client/src/components/modal/review/review-edit/ReviewEdit.tsx
@@ -44,7 +44,10 @@ const ReviewEdit = ({
     }
   };
   const handleSubmitAll = () => {
-    if (!content) return;
+    if (!content.replace(/<p><br><\/p>/g, '')) {
+      alert('내용을 입력해 주세요.');
+      return;
+    }
     handleSubmit(onSubmit)();
   };
   return (
@@ -54,20 +57,6 @@ const ReviewEdit = ({
           setIsEditing(false);
         }}
       />
-      {/* <H1Title.H1>
-          <H1Title.H1span>후기</H1Title.H1span>
-        </H1Title.H1> */}
-      {/* <S.Poster src={performance?.imageUrl} /> 
-        <S.Summary>
-          <p>공연명</p>
-          <p>{performance?.title || '공연 제목'}</p>
-          <p>날짜</p>
-          <p>
-            {performance?.date
-              ? getDateTime(performance.date)
-              : '날짜 불러오기 실패'}
-          </p>
-            </S.Summary> */}
       <S.Heading3>제목</S.Heading3>
       <S.Form onSubmit={handleSubmit(onSubmit)}>
         <Controller
@@ -76,14 +65,19 @@ const ReviewEdit = ({
           defaultValue={defaultTitle || review.reviewTitle}
           rules={{
             required: '반드시 입력해야 합니다',
+            minLength: {
+              value: 5,
+              message: '다섯 글자 이상 입력해 주세요.',
+            },
           }}
-          render={({ field }) => {
+          render={({ field, fieldState: { error } }) => {
             return (
               <Input
                 height={30}
                 width={285}
                 onChange={field.onChange}
                 value={field.value}
+                errorMessage={error?.message}
               />
             );
           }}
