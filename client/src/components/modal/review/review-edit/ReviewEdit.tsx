e91aea69f70d5b89dacd78c2591eb1756b79bba5
mule-heo <zaq98114@gmail.com>
2023-08-03 20:55:52 +0900

    Fix: import 경로 수정

--- client/src/components/modal/review/review-edit/ReviewEdit.tsx
+++ client/src/components/modal/review/review-edit/ReviewEdit.tsx
@@ -1,8 +1,8 @@
 import S from './ReviewEdit.style';
 import { Button } from '../../../buttons/Buttons';
 import { Input } from '../../../inputs/Inputs';
-import { Editor } from '../../../inputs/editor/Editor';
-import { useEditorStore } from '../../../inputs/editor/EditorStore';
+import { Editor } from '../../../editor/Editor';
+import { useEditorStore } from '../../../editor/EditorStore';
 import { useForm, SubmitHandler, Controller } from 'react-hook-form';
 import { patchReview } from '../../../../api/fetchAPI';
 import { ReactComponent as ArrowIcon } from '../../../../icons/icon_right.svg';
